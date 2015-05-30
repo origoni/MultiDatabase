674a27034d54445a722776af110151f76b04c7c3
DK Lee <origoni@daum.net>
2015-05-30 21:27:05 +0900

    article

--- src/main/java/com/millky/dev/database/multi/domain/article/Article.java
+++ src/main/java/com/millky/dev/database/multi/domain/article/Article.java
@@ -1,5 +1,16 @@
 package com.millky.dev.database.multi.domain.article;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Article {
 
+	@Id
+	@GeneratedValue
+	int id;
+	
+	String note;
+	
 }
